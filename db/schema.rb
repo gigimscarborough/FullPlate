26813f762cc552ccdb602d7c623d2f285f257ab9
gscarborough <gigimscarborough@gmail.com>
2021-01-19 19:33:24 -0500

    added cities table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_163359) do
+ActiveRecord::Schema.define(version: 2021_01_20_002926) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,16 @@ ActiveRecord::Schema.define(version: 2021_01_18_163359) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "cities", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "state", null: false
+    t.string "country", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_cities_on_name"
+    t.index ["state"], name: "index_cities_on_state"
   end
 
   create_table "restaurants", force: :cascade do |t|
