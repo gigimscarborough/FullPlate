1c7dcb9f26dd9c75605ad4dffb25d567b9e18d6b
gscarborough <gigimscarborough@gmail.com>
2021-01-19 19:17:12 -0500

    temp remove city_id

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-    validates :name, :city_id, :address, :phone_number, :description, :opening_time, :closing_time, :price_range, :cuisine_type, presence: true
+    validates :name, :address, :phone_number, :description, :opening_time, :closing_time, :price_range, :cuisine_type, presence: true
 
     has_many_attached: :images
 end
