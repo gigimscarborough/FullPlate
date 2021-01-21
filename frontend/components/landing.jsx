7f3184a95689361b2ce8abe0e9fbaeda8d7f8901
gscarborough <gigimscarborough@gmail.com>
2021-01-21 17:58:30 -0500

    added splash image

--- frontend/components/landing.jsx
+++ frontend/components/landing.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import SplashNavBar from '../components/navbar/splash_navbar'
-
+import Search from './search/search'
 class Landing extends React.Component{
 
     render(){
         return(
             <div>
                 <SplashNavBar/>
+                <Search/>
             </div>
         )
     }
