b7a703accb2710716086eebe98357e8e02cee6cf
gscarborough <gigimscarborough@gmail.com>
2021-01-20 23:18:22 -0500

    completed splash nav bar

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -13,8 +13,12 @@ class Greeting extends React.Component {
             
             return (
                     <div className="logged-links">
-                        <h1>Welcome, {this.props.currentUser.first_name}!</h1>
-                        <button onClick={this.props.logout.bind(this)}>Log Out</button>
+                        <Link onClick={this.props.logout.bind(this)}><i class="far fa-user-circle"></i></Link>
+                        <Link><i class="far fa-calendar"></i></Link>
+                        <Link><i class="far fa-bell"></i></Link>
+                        <Link className="search-icon lsearch" to="/"><img src={window.search_icon} /></Link>
+                        {/* <h1>Welcome, {this.props.currentUser.first_name}!</h1>
+                        <button onClick={this.props.logout.bind(this)}>Log Out</button> */}
                     </div>
             )
         } else {
