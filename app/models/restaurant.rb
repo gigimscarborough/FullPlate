0be317e6966d94f35cc40632ee8ff7ecfa53c740
gscarborough <gigimscarborough@gmail.com>
2021-01-22 02:07:35 -0500

    fixed syntax

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ApplicationRecord
-    validates :name, :address, :city_id:phone_number, :description, :opening_time, :closing_time, :price_range, :cuisine_type, presence: true
+    validates :name, :address, :city_id, :phone_number, :description, :opening_time, :closing_time, :price_range, :cuisine_type, presence: true
 
     has_many_attached :images
 end
