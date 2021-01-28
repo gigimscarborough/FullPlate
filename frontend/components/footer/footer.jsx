f4d906eabb6f784bc68f68bf796c3fcd796a7058
gscarborough <gigimscarborough@gmail.com>
2021-01-28 16:47:17 -0500

    css footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -9,27 +9,27 @@ const Footer = () => {
                 <div className="footer-header">
                     <h2>GIGI SCARBOROUGH</h2>
                         <h3>JOIN ME ON</h3>
-                    <div>
+                    <div className="ftr-links">
                         <div>
                             <a href="https://github.com/gigimscarborough">
-                                <i class="fas fa-circle"></i>
-                                <i class="fab fa-github-square"></i>
+                                <i class=" ftl fc fas fa-circle"></i>
+                                <i class=" ftl fi fab fa-github-square"></i>
                             </a>
                         </div>
                         <div>
                             <a href="https://angel.co/u/gigi-scarborough">
-                                <i class="fas fa-circle"></i>
-                                <i class="fab fa-angellist"></i>
+                                <i class="ftl fc fas fa-circle"></i>
+                                <i class="ftl fi fab fa-angellist"></i>
                             </a>
                         </div>
                         <div>
                             <a href="https://linkedin.com/in/gigimscarborough">
-                                <i class="fas fa-circle"></i>
-                                <i class="fab fa-linkedin-in"></i>
+                                <i class="ftl fc fas fa-circle"></i>
+                                <i class="ftl fi fab fa-linkedin-in"></i>
                             </a>
                         </div>
-                    </div>
                 </div>
+                    </div>
                 <div>
 
                 </div>
