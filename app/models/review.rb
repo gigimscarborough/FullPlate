6feb54c677945a7f47db9336312666ad17fb313f
gscarborough <gigimscarborough@gmail.com>
2021-02-22 18:30:40 -0500

    created review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,13 @@
 class Review < ApplicationRecord
+    validates :restaurant_id, :guest_id, :body, :visited, :food_rating, :service_rating, :ambience_rating, :value_rating, :overall_rating, :rating, :would_recommend, presence:true
+    
+    
+    belongs_to :user,
+    foreign_key: :guest_id,
+    class_name: :User
+    
+    belongs_to :restaurant,
+    foreign_key: :restaurant_id,
+    class_name: :Restaurant
+    
 end
