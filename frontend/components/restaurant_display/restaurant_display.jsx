4a0e19b14f29265e2391af8f89d2ac7dafd46e96
gscarborough <gigimscarborough@gmail.com>
2021-02-21 20:21:40 -0500

    splash page layout fix

--- frontend/components/restaurant_display/restaurant_display.jsx
+++ frontend/components/restaurant_display/restaurant_display.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 import RestaurantIndexContainer from './restaurant_index_container'
 
-class RestaurantDisplay extends React.Component{
-    
-    render (){
+class RestaurantDisplay extends React.Component {
+
+    render() {
         const location = !this.props.currentUser ? "New York" : this.props.currentUser.dining_city
-        
+
         return (
-            <div className="restaurant-display">
-                <div className="display-location">
-                    <p>It looks like you're in {location}. </p>
+            <div className="restaurant-display-hold">
+                <div className="restaurant-display">
+                    <div className="display-location">
+                        <p>It looks like you're in {location}. </p>
+                    </div>
+                    <RestaurantIndexContainer />
                 </div>
-                <RestaurantIndexContainer/>
             </div>
         )
     }
