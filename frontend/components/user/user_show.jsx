a6f4c1984035a3eee7525d40b6e2c0ba559e40c2
gscarborough <gigimscarborough@gmail.com>
2021-02-23 13:29:05 -0500

    fav styling

--- frontend/components/user/user_show.jsx
+++ frontend/components/user/user_show.jsx
@@ -9,6 +9,7 @@ class UserShow extends React.Component {
 
         this.reservations = this.reservations.bind(this)
         this.pastRes = this.pastRes.bind(this)
+        this.savedRests = this.savedRests.bind(this)
     }
 
     componentDidMount() {
@@ -22,10 +23,10 @@ class UserShow extends React.Component {
         const allRests = Object.values(this.props.restaurants)
         const rests = this.props.currentUser.reserved_restaurants
 
-        
+
         const reservations = this.props.currentUser.reservations.filter(res => new Date(res.reservation_datetime) > today)
         const resList = []
-        
+
         debugger
 
         for (let i = 0; i < reservations.length; i++) {
@@ -34,10 +35,10 @@ class UserShow extends React.Component {
 
             resList.push(
                 (
-                    <div className="reservation-it">
+                    <div key={i} className="reservation-it">
                         <div>
                             {/* <Link to={`/restaurants/${reservations[i].restaurant_id}`}><img id="u-res-pic" src={allRests.filter(rest => (rest.id === reservations[i].restaurant_id))[0].photoUrls[0]} alt="" /></Link> */}
-                            <Link to={`/restaurants/${reservations[i].restaurant_id}`}><img id="u-res-pic" src={ window.salmonplate } alt="" /></Link>
+                            <Link to={`/restaurants/${reservations[i].restaurant_id}`}><img id="u-res-pic" src={window.salmonplate} alt="" /></Link>
                         </div>
                         <div className="u-res-info-div">
                             <div className="u-res-name">
@@ -73,7 +74,7 @@ class UserShow extends React.Component {
         const today = new Date()
         const allRests = Object.values(this.props.restaurants)
         const rests = this.props.currentUser.reserved_restaurants
-    
+
 
         const reservations = this.props.currentUser.reservations.filter(res => new Date(res.reservation_datetime) < today)
         const resList = []
@@ -89,16 +90,16 @@ class UserShow extends React.Component {
                     <span>Save this restaurant</span>
                 </div>
             ) : (
-                    <div onClick={() => this.props.deleteFavorite(thisFav.id).then(() => this.props.fetchUser(this.props.currentUser.id))}className="fav-me">
+                    <div onClick={() => this.props.deleteFavorite(thisFav.id).then(() => this.props.fetchUser(this.props.currentUser.id))} className="fav-me">
                         <i className="fas fa-bookmark"></i>
                         <span>Restaurant saved</span>
                     </div>
                 )
-                debugger
+            debugger
             resList.push(
                 (
 
-                    <div className="reservation-it">
+                    <div key={i} className="reservation-it">
                         <div>
                             {/* <Link to={`/restaurants/${reservations[i].restaurant_id}`}><img id="u-res-pic" src={allRests.filter(rest => (rest.id === reservations[i].restaurant_id))[0].photoUrls[0]} alt="" /></Link> */}
                             <Link to={`/restaurants/${reservations[i].restaurant_id}`}><img id="u-res-pic" src={window.salmonplate} alt="" /></Link>
@@ -134,25 +135,75 @@ class UserShow extends React.Component {
         return resList
     }
 
-    
+    savedRests() {
+
+        const allRests = Object.values(this.props.restaurants)
+        const favs = this.props.currentUser.favorites.map(fav => fav.restaurant_id)
+        const theseFavs = allRests.filter(rest => favs.includes(rest.id))
+
+        const favsList = []
+
+        for (let i = 0; i < theseFavs.length; i++) {
+            favsList.push(
+                <div key={i} className="fav-hold">
+                    <div  className="fav-it">
+                        <div>
+                            {/* <Link to={`/restaurants/${theseFavs[i].id}`}><img id="u-res-pic" src={theseFavs[i].photoUrls[0]} alt="" /></Link> */}
+                            <Link to={`/restaurants/${theseFavs[i].id}`}><img id="u-res-pic" src={window.salmonplate} alt="" /></Link>
+                        </div>
+                        <div className="u-res-info-div">
+                            <div className="u-res-name">
+                                <Link to={`/restaurants/${theseFavs[i].restaurant_id}`}><span>{theseFavs[i].name}</span></Link>
+                            </div>
+                            <div onClick={() => this.props.deleteFavorite(thisFav.id).then(() => this.props.fetchUser(this.props.currentUser.id))} id="rem-fav">
+                                <i className="fas fa-bookmark"></i>
+                                <span>Remove from saved restaurants</span>
+                            </div>
+                            <span className="icn-strs4">
+                                <i className="fas fa-star ffpst"></i>
+                                <i className="fas fa-star ffpst"></i>
+                                <i className="fas fa-star ffpst"></i>
+                                <i className="fas fa-star ffpst"></i>
+                                <i className="fas fa-star-half ffpstr"></i>
+                                <i className="fas fa-star-half ffpstl"></i>
+                            </span>
+                            <div className="fav-det">
+                                <span>{theseFavs[i].cuisine_type}</span> <div id="fav-brdr"></div><span>{theseFavs[i].neighborhood}</span>
+                            </div>
+
+                        </div>
+                    </div>
+                     <button id="fav-reserve">Reserve Now</button>
+
+                 </div>
+            )
+
+        }
+
+        return favsList
+
+
+    }
+
+
 
 
 
     render() {
-       
+
 
         if (Object.values(this.props.restaurants) <= 0) {
             return null
-        } 
-        
-        if (typeof this.props.restaurants === 'undefined'){
+        }
+
+        if (typeof this.props.restaurants === 'undefined') {
             return null
         }
 
         if (typeof this.props.reservations === 'undefined') {
             return null
         }
-        
+
         else {
 
 
@@ -232,7 +283,9 @@ class UserShow extends React.Component {
                                     </h1>
                                     </div>
                                     <div className="btm">
-                                        <p>You have no favorite restaurants to show on this list.</p>
+                                        <div className="past-hold">
+                                            {this.savedRests() === 0 ? <p>You have no favorite restaurants to show on this list.</p> : this.savedRests()}
+                                        </div>
                                     </div>
                                 </div >
                                 <div id="pts" className="usdiv us-up">
